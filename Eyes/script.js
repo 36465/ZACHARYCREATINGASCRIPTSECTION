d26841d3505086e704c2d852b3f1a7142ac8a86c
36465 <36465@rpsk12.org>
2025-03-20 13:53:50 -0400

    Done

--- Eyes/script.js
+++ Eyes/script.js
@@ -16,3 +16,23 @@ var toggleTransition = function() {
 
   running = !running;
 }
+
+var boxOnebtm = document.getElementsByClassName('box3btm')[0];
+var boxTwobtm = document.getElementsByClassName('box3btm')[1];
+
+var running = false;
+
+var toggleTransitionbtm = function() {
+  if(!running) 
+  { 
+    boxOnebtm.classList.add('horizTranslatebtm');
+    boxTwobtm.classList.add('horizTranslatebtm');
+  } else {
+    boxOnebtm.classList.remove('horizTranslatebtm'); 
+     boxTwobtm.classList.remove('horizTranslatebtm');
+    
+  }  
+
+  running = !running;
+}
+
--- GeeksforGeeks Star/script.js
+++ GeeksforGeeks Star/script.js
@@ -11,3 +11,17 @@ var toggleTransition = function() {
 
   running = !running;
 }
+
+var boxOnebtm = document.getElementsByClassName('boxbtm')[0];
+var running = false;
+
+var toggleTransitionbtm = function() {
+  if(!running) 
+  { 
+    boxOnebtm.classList.add('horizTranslatebtm');
+  } else {
+    boxOnebtm.classList.remove('horizTranslatebtm'); 
+  }  
+
+  running = !running;
+}
